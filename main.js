0f4317d030305a257e066af0bf034c24d21a37cf
Kimura-Kyoko <75403536+Kimura-Kyoko@users.noreply.github.com>
2021-01-07 16:37:57 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
-let insertY = ['the soup kitchen','Disneyland','the White House'];
-let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+let storyText = '昔々あるところにおじいさんとおばあさんがいました。 :insertx: はある日津田梅子さんのお墓の前で :inserty:を拾いました。名前をBobと名付けました。 :insertx: はその子を我が子のように大切に育てました。その子は大変:insertz:成長しました。';
+let insertX = ['おじいさん','おばあさん','おじいさんの友人'];
+let insertY = ['かわいい赤子','可愛らしい子犬','可愛らしい子猫'];
+let insertZ = ['美しく','たくましく','個性的に'];
 
 randomize.addEventListener('click', result);
 
@@ -31,7 +31,7 @@ function result() {
     newStory = newStory.replace('Bob',name);
   }
 
-  if(document.getElementById("uk").checked) {
+  if(document.getElementById("jp").checked) {
     const weight = Math.round(300*0.0714286) + ' stone';
     const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
     newStory = newStory.replace('94 fahrenheit',temperature);
